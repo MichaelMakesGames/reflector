1608f68cb22e659ec3026b9b9e46fc86e933430c
Michael Moore <mscottmoore@protonmail.com>
2019-10-23 21:41:13 -0500

    Basic tooltips for actions

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,18 +1,22 @@
 /* global document */
+import Tooltip from "rc-tooltip";
+import "rc-tooltip/assets/bootstrap.css";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { DOWN, LEFT, PLAYER_ID, RIGHT, UP } from "~/constants";
 import * as actions from "~/state/actions";
 import * as selectors from "~/state/selectors";
-import { Entity, Pos } from "~/types/Entity";
-import { PLAYER_ID, UP, LEFT, DOWN, RIGHT } from "~/constants";
 import { Action } from "~/types/Action";
+import { Entity, Pos, Description } from "~/types/Entity";
 import buildings from "~data/buildings";
+import { createEntityFromTemplate } from "~utils/entities";
 
 interface Control {
   key: string;
   action: Action;
   label: string;
   hidden?: boolean;
+  tooltip?: string;
 }
 function getControls(
   activeWeapon: Entity | null,
@@ -72,6 +76,8 @@ function getControls(
       key: "Enter",
       action: actions.activateWeapon({ slot: 1 }),
       label: "Activate Laser",
+      tooltip:
+        "Activates your laser. You will get to choose your direction and preview before firing.",
     },
   ];
   const reflectorActions = [
@@ -82,11 +88,14 @@ function getControls(
         takesTurn: false,
       }),
       label: "Place Reflector (free action)",
+      tooltip:
+        "Reflectors are your main tool for manipulating lasers. Placing a reflector does not cost any resources and does not take a turn. However, reflectors can only be placed around you or a projector, and are automatically destroyed if they are ever out of range.",
     },
     {
       key: "c",
       action: actions.clearReflectors(),
       label: "Clear Reflectors (free action)",
+      tooltip: "This removes all reflectors from the map.",
     },
   ];
   const wait = [{ key: ".", action: actions.playerTookTurn(), label: "Wait" }];
@@ -128,6 +137,8 @@ function getControls(
           validitySelector: building.validitySelector,
         }),
         label: `${building.label} (${building.cost.amount} ${building.cost.resource})`,
+        tooltip: (createEntityFromTemplate(building.template)
+          .description as Description).description,
       })),
     ];
   }
@@ -355,6 +366,15 @@ export default function Controls() {
           >
             <kbd>{control.key}</kbd>
             <span>{control.label}</span>
+            {control.tooltip && (
+              <Tooltip
+                overlay={<span>{control.tooltip}</span>}
+                placement="top"
+                overlayStyle={{ pointerEvents: "none", maxWidth: "20em" }}
+              >
+                <span className="help">?</span>
+              </Tooltip>
+            )}
           </button>
         ))}
     </div>
