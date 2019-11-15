24a88af3543cf5ecda8c641cd247db69f5f69e1f
Michael Moore <mscottmoore@protonmail.com>
2019-11-14 22:15:56 -0600

    Refactor controls; enable arrows and numpad for movement

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -12,7 +12,8 @@ import buildings from "~data/buildings";
 import { createEntityFromTemplate } from "~utils/entities";
 
 interface Control {
-  key: string;
+  display: string;
+  triggers: { code: string; shift?: boolean }[];
   action: Action;
   label: string;
   hidden?: boolean;
@@ -25,40 +26,69 @@ function getControls(
   isBuildMenuOpen: boolean,
   inspector: Entity | null,
 ): Control[] {
+  const upTriggers = [
+    { code: "KeyW" },
+    { code: "ArrowUp" },
+    { code: "Numpad8" },
+  ];
+  const downTriggers = [
+    { code: "KeyS" },
+    { code: "ArrowDown" },
+    { code: "Numpad2" },
+  ];
+  const leftTriggers = [
+    { code: "KeyA" },
+    { code: "ArrowLeft" },
+    { code: "Numpad4" },
+  ];
+  const rightTriggers = [
+    { code: "KeyD" },
+    { code: "ArrowRight" },
+    { code: "Numpad6" },
+  ];
+  const confirmTriggers = [{ code: "Enter" }, { code: "Space" }];
+  const cancelTriggers = [{ code: "Escape" }];
+
   const movePlayer = [
     {
-      key: "w",
+      display: "w",
+      triggers: upTriggers,
       action: actions.move({ entityId: PLAYER_ID, ...UP }),
       label: "Move Up",
     },
     {
-      key: "a",
+      display: "a",
+      triggers: leftTriggers,
       action: actions.move({ entityId: PLAYER_ID, ...LEFT }),
       label: "Move Left",
     },
     {
-      key: "s",
+      display: "s",
+      triggers: downTriggers,
       action: actions.move({ entityId: PLAYER_ID, ...DOWN }),
       label: "Move Down",
     },
     {
-      key: "d",
+      display: "d",
+      triggers: rightTriggers,
       action: actions.move({ entityId: PLAYER_ID, ...RIGHT }),
       label: "Move Right",
     },
   ];
-  const activateWeapon = [
+  const activateWeapon: Control[] = [
     {
-      key: "Enter",
+      display: "Enter",
+      triggers: confirmTriggers,
       action: actions.activateWeapon({ slot: 1 }),
       label: "Activate Laser",
       tooltip:
         "Activates your laser. You will get to choose your direction and preview before firing.",
     },
   ];
-  const reflectorActions = [
+  const reflectorActions: Control[] = [
     {
-      key: "r",
+      display: "r",
+      triggers: [{ code: "KeyR" }],
       action: actions.activatePlacement({
         template: "REFLECTOR_UP_RIGHT",
         takesTurn: false,
@@ -68,30 +98,41 @@ function getControls(
         "Reflectors are your main tool for manipulating lasers. Placing a reflector does not cost any resources and does not take a turn. However, reflectors can only be placed around you or a projector, and are automatically destroyed if they are ever out of range.",
     },
     {
-      key: "c",
+      display: "c",
+      triggers: [{ code: "KeyC" }],
       action: actions.clearReflectors(),
       label: "Clear Reflectors (free action)",
       tooltip: "This removes all reflectors from the map.",
     },
   ];
-  const wait = [{ key: ".", action: actions.playerTookTurn(), label: "Wait" }];
-  const build = [
+  const wait: Control[] = [
     {
-      key: "b",
+      display: ".",
+      triggers: [{ code: "Period" }],
+      action: actions.playerTookTurn(),
+      label: "Wait",
+    },
+  ];
+  const build: Control[] = [
+    {
+      display: "b",
+      triggers: [{ code: "KeyB" }],
       action: actions.openBuildMenu(),
       label: "Build",
     },
   ];
-  const mine = [
+  const mine: Control[] = [
     {
-      key: "m",
+      display: "m",
+      triggers: [{ code: "KeyM" }],
       action: actions.mine(),
       label: "Mine",
     },
   ];
-  const inspect = [
+  const inspect: Control[] = [
     {
-      key: "?",
+      display: "q",
+      triggers: [{ code: "KeyQ" }],
       action: actions.inspect(),
       label: "Inspect (free action)",
     },
@@ -100,12 +141,14 @@ function getControls(
   if (isBuildMenuOpen) {
     return [
       {
-        key: "Escape",
+        display: "Escape",
+        triggers: cancelTriggers,
         action: actions.closeBuildMenu(),
         label: "Close",
       },
       ...buildings.map(building => ({
-        key: building.key,
+        display: building.key,
+        triggers: [{ code: `Key${building.key.toUpperCase()}` }],
         action: actions.activatePlacement({
           template: building.template,
           cost: building.cost,
@@ -122,28 +165,38 @@ function getControls(
   if (activeWeapon) {
     return [
       {
-        key: "w",
+        display: "w",
+        triggers: upTriggers,
         action: actions.targetWeapon(UP),
         label: "Target Up",
       },
       {
-        key: "a",
+        display: "a",
+        triggers: leftTriggers,
         action: actions.targetWeapon(LEFT),
         label: "Target Left",
       },
       {
-        key: "s",
+        display: "s",
+        triggers: downTriggers,
         action: actions.targetWeapon(DOWN),
         label: "Target Down",
       },
       {
-        key: "d",
+        display: "d",
+        triggers: rightTriggers,
         action: actions.targetWeapon(RIGHT),
         label: "Target Right",
       },
-      { key: "Enter", action: actions.fireWeapon(), label: "Fire" },
       {
-        key: "Escape",
+        display: "Enter",
+        triggers: confirmTriggers,
+        action: actions.fireWeapon(),
+        label: "Fire",
+      },
+      {
+        display: "Escape",
+        triggers: cancelTriggers,
         action: actions.activateWeapon({
           slot: activeWeapon.weapon ? activeWeapon.weapon.slot : 0,
         }),
@@ -155,27 +208,32 @@ function getControls(
   if (inspector) {
     return [
       {
-        key: "Escape",
+        display: "Escape",
+        triggers: cancelTriggers,
         action: actions.cancelInspect(),
         label: "Close",
       },
       {
-        key: "w",
+        display: "w",
+        triggers: upTriggers,
         action: actions.move({ entityId: inspector.id, ...UP }),
         label: "Move Inspector Up",
       },
       {
-        key: "a",
+        display: "a",
+        triggers: leftTriggers,
         action: actions.move({ entityId: inspector.id, ...LEFT }),
         label: "Move Inspector Left",
       },
       {
-        key: "s",
+        display: "s",
+        triggers: downTriggers,
         action: actions.move({ entityId: inspector.id, ...DOWN }),
         label: "Move Inspector Down",
       },
       {
-        key: "d",
+        display: "d",
+        triggers: rightTriggers,
         action: actions.move({ entityId: inspector.id, ...RIGHT }),
         label: "Move Inspector Right",
       },
@@ -185,57 +243,81 @@ function getControls(
   if (placing && placing.placing) {
     const placingControls: Control[] = [
       {
-        key: "w",
+        display: "w",
+        triggers: upTriggers,
         action: actions.movePlacement({ direction: UP, jumpToValid: false }),
         label: "Move Target Up (shift to jump to valid spot)",
       },
       {
-        key: "a",
+        display: "a",
+        triggers: leftTriggers,
         action: actions.movePlacement({ direction: LEFT, jumpToValid: false }),
         label: "Move Target Left (shift to jump to valid spot)",
       },
       {
-        key: "s",
+        display: "s",
+        triggers: downTriggers,
         action: actions.movePlacement({ direction: DOWN, jumpToValid: false }),
         label: "Move Target Down (shift to jump to valid spot)",
       },
       {
-        key: "d",
+        display: "d",
+        triggers: rightTriggers,
         action: actions.movePlacement({ direction: RIGHT, jumpToValid: false }),
         label: "Move Target Right (shift to jump to valid spot)",
       },
       {
-        key: "W",
+        display: "W",
+        triggers: upTriggers.map(t => ({ ...t, shift: true })),
         action: actions.movePlacement({ direction: UP, jumpToValid: true }),
         hidden: true,
         label: "Move Target Up",
       },
       {
-        key: "A",
+        display: "A",
+        triggers: leftTriggers.map(t => ({ ...t, shift: true })),
         action: actions.movePlacement({ direction: LEFT, jumpToValid: true }),
         hidden: true,
         label: "Move Target Left",
       },
       {
-        key: "S",
+        display: "S",
+        triggers: downTriggers.map(t => ({ ...t, shift: true })),
         action: actions.movePlacement({ direction: DOWN, jumpToValid: true }),
         hidden: true,
         label: "Move Target Down",
       },
       {
-        key: "D",
+        display: "D",
+        triggers: rightTriggers.map(t => ({ ...t, shift: true })),
         action: actions.movePlacement({ direction: RIGHT, jumpToValid: true }),
         hidden: true,
         label: "Move Target Right",
       },
-      { key: "r", action: actions.rotatePlacement(), label: "Rotate" },
-      { key: "Escape", action: actions.cancelPlacement(), label: "Cancel" },
-      { key: "Enter", action: actions.finishPlacement(), label: "Confirm" },
+      {
+        display: "r",
+        triggers: [{ code: "KeyR" }],
+        action: actions.rotatePlacement(),
+        label: "Rotate",
+      },
+      {
+        display: "Escape",
+        triggers: cancelTriggers,
+        action: actions.cancelPlacement(),
+        label: "Cancel",
+      },
+      {
+        display: "Enter",
+        triggers: confirmTriggers,
+        action: actions.finishPlacement(),
+        label: "Confirm",
+      },
     ];
 
     if (placing.reflector) {
       placingControls.push({
-        key: "Backspace",
+        display: "Backspace",
+        triggers: [{ code: "Backspace" }],
         action: actions.removeReflector(),
         label: "Remove Reflector",
       });
@@ -268,15 +350,17 @@ export default function Controls() {
   const controls: Control[] = gameOver
     ? []
     : getControls(activeWeapon, pos, placing, isBuildMenuOpen, inspector);
-  const keyMap: { [key: string]: Action } = controls.reduce(
-    (acc, cur) => ({ ...acc, [cur.key]: cur.action }),
-    {},
-  );
 
   function listener(event: KeyboardEvent) {
-    const { key } = event;
-    if (!gameOver && keyMap[key]) {
-      dispatch(keyMap[key]);
+    if (gameOver) return;
+    const { code, shiftKey } = event;
+    const triggered = controls.find(control =>
+      control.triggers.some(
+        trigger => trigger.code === code && Boolean(trigger.shift) === shiftKey,
+      ),
+    );
+    if (triggered) {
+      dispatch(triggered.action);
     }
   }
 
@@ -293,12 +377,12 @@ export default function Controls() {
         .map(control => (
           <button
             type="button"
-            key={control.key}
+            key={control.display}
             className="control"
             onClick={() => dispatch(control.action)}
             onFocus={e => e.target.blur()}
           >
-            <kbd>{control.key}</kbd>
+            <kbd>{control.display}</kbd>
             <span>{control.label}</span>
             {control.tooltip && (
               <Tooltip
