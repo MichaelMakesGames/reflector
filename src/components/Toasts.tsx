ec990aaf8e178908d592db1b487bf5ca981e8ff4
Michael Moore <mscottmoore@protonmail.com>
2020-07-28 22:07:25 -0500

    Fix toasts

--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useContext } from "react";
 import { Notyf } from "notyf";
 import "notyf/notyf.min.css";
+import React, { useContext, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import selectors from "~state/selectors";
 
@@ -8,45 +8,43 @@ const NotyfContext = React.createContext(
   new Notyf({
     position: { x: "right", y: "top" },
     duration: 5000,
+    dismissible: true,
   }),
 );
 
 export default function Toasts() {
+  const notyf = useContext(NotyfContext);
   const messageLog = useSelector(selectors.messageLog);
   const thisTurn = useSelector(selectors.turn);
   const lastTurn = thisTurn - 1;
   const thisTurnMessages = messageLog[thisTurn] || [];
   const lastTurnMessages = messageLog[lastTurn] || [];
-  return (
-    <>
-      {thisTurnMessages.map(({ message, type }, i) => (
-        <Toast
-          // eslint-disable-next-line react/no-array-index-key
-          key={`${thisTurn}_${i}`}
-          message={message}
-          type={type || "error"}
-        />
-      ))}
-      {lastTurnMessages.map(({ message, type }, i) => (
-        <Toast
-          // eslint-disable-next-line react/no-array-index-key
-          key={`${lastTurn}_${i}`}
-          message={message}
-          type={type || "error"}
-        />
-      ))}
-    </>
-  );
-}
+  const toastedRef = useRef<Set<string>>(new Set());
+  const toasted = toastedRef.current;
 
-export function Toast({ message, type }: { message: string; type: string }) {
-  const notyf = useContext(NotyfContext);
   useEffect(() => {
-    const notification = notyf.open({
-      type,
-      message,
+    thisTurnMessages.forEach(({ message, type }, i) => {
+      const id = `${thisTurn}_${i}`;
+      if (!toasted.has(id)) {
+        toasted.add(id);
+        notyf.open({
+          type: type || "error",
+          message,
+        });
+      }
     });
-    return () => notyf.dismiss(notification);
+
+    lastTurnMessages.forEach(({ message, type }, i) => {
+      const id = `${lastTurn}_${i}`;
+      if (!toasted.has(id)) {
+        toasted.add(id);
+        notyf.open({
+          type: type || "error",
+          message,
+        });
+      }
+    });
   });
+
   return null;
 }
