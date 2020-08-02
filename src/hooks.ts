9a8141158c501d04f260533cc5f4893fe9e1af07
Michael Moore <mscottmoore@protonmail.com>
2020-08-02 15:49:00 -0500

    Prevent default on all keyboard shortcuts

--- src/hooks.ts
+++ src/hooks.ts
@@ -6,12 +6,25 @@ export function useShortcuts(
   enabled: boolean = true,
 ): clack.Group {
   let group = clack.group({});
+
   useEffect(() => {
-    group = clack.group(shortcuts);
+    // prevent defaults to disable browser shortcuts where possible
+    const shortcutsWithPreventDefault = Object.entries(shortcuts).reduce<
+      Record<string, (e: KeyboardEvent) => void>
+    >((acc, cur) => {
+      acc[cur[0]] = (e: KeyboardEvent) => {
+        e.preventDefault();
+        cur[1](e);
+      };
+      return acc;
+    }, {});
+
+    group = clack.group(shortcutsWithPreventDefault);
     group.enabled = enabled;
     return () => {
       group.enabled = false;
     };
   });
+
   return group;
 }
