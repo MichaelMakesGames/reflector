8d20a4636f20f9dcebf3ced94b17976606337588
Michael Moore <mscottmoore@protonmail.com>
2021-01-19 20:16:24 -0600

    Fix bug moving cursor while aiming

--- src/components/Laser.tsx
+++ src/components/Laser.tsx
@@ -47,24 +47,40 @@ export default function Laser() {
     code: ControlCode.Up,
     group: HotkeyGroup.Main,
     callback: makeAimHandler(UP, settings.aimInSameDirectionToFire),
+    alt: false,
+    ctrl: false,
+    meta: false,
+    shift: false,
     [settings.aimingModifierKey]: true,
   });
   useControl({
     code: ControlCode.Down,
     group: HotkeyGroup.Main,
     callback: makeAimHandler(DOWN, settings.aimInSameDirectionToFire),
+    alt: false,
+    ctrl: false,
+    meta: false,
+    shift: false,
     [settings.aimingModifierKey]: true,
   });
   useControl({
     code: ControlCode.Left,
     group: HotkeyGroup.Main,
     callback: makeAimHandler(LEFT, settings.aimInSameDirectionToFire),
+    alt: false,
+    ctrl: false,
+    meta: false,
+    shift: false,
     [settings.aimingModifierKey]: true,
   });
   useControl({
     code: ControlCode.Right,
     group: HotkeyGroup.Main,
     callback: makeAimHandler(RIGHT, settings.aimInSameDirectionToFire),
+    alt: false,
+    ctrl: false,
+    meta: false,
+    shift: false,
     [settings.aimingModifierKey]: true,
   });
 
@@ -73,24 +89,40 @@ export default function Laser() {
     group: HotkeyGroup.Main,
     callback: makeAimHandler(UP, settings.aimInSameDirectionToFire),
     disabled: !(settings.unmodifiedAiming && isWeaponActive),
+    alt: false,
+    ctrl: false,
+    meta: false,
+    shift: false,
   });
   useControl({
     code: ControlCode.Down,
     group: HotkeyGroup.Main,
     callback: makeAimHandler(DOWN, settings.aimInSameDirectionToFire),
     disabled: !(settings.unmodifiedAiming && isWeaponActive),
+    alt: false,
+    ctrl: false,
+    meta: false,
+    shift: false,
   });
   useControl({
     code: ControlCode.Left,
     group: HotkeyGroup.Main,
     callback: makeAimHandler(LEFT, settings.aimInSameDirectionToFire),
     disabled: !(settings.unmodifiedAiming && isWeaponActive),
+    alt: false,
+    ctrl: false,
+    meta: false,
+    shift: false,
   });
   useControl({
     code: ControlCode.Right,
     group: HotkeyGroup.Main,
     callback: makeAimHandler(RIGHT, settings.aimInSameDirectionToFire),
     disabled: !(settings.unmodifiedAiming && isWeaponActive),
+    alt: false,
+    ctrl: false,
+    meta: false,
+    shift: false,
   });
 
   return (
