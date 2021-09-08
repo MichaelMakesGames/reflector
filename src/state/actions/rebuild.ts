7bcc14384b3c10dae9fe9dffd6991139524479ed
Michael Moore <mscottmoore@protonmail.com>
2021-09-08 18:01:41 -0500

    Check building conditions when rebuilding from rubble

--- src/state/actions/rebuild.ts
+++ src/state/actions/rebuild.ts
@@ -4,6 +4,7 @@ import WrappedState from "../../types/WrappedState";
 import { createEntityFromTemplate } from "../../lib/entities";
 import resources from "../../data/resources";
 import audio from "../../lib/audio";
+import { areConditionsMet } from "../../lib/conditions";
 
 const rebuild = createAction("rebuild")<string>();
 export default rebuild;
@@ -14,8 +15,23 @@ function rebuildHandler(
 ): void {
   const entity = state.select.entityById(action.payload);
   if (!entity || !entity.rebuildable || !entity.pos) return;
-  const blueprint = createEntityFromTemplate(entity.rebuildable.blueprint);
+  const blueprint = createEntityFromTemplate(entity.rebuildable.blueprint, {
+    pos: entity.pos,
+  });
   if (!blueprint.blueprint) return;
+
+  const failedConditions = blueprint.blueprint.validityConditions.filter(
+    (validityCondition) =>
+      !areConditionsMet(state, blueprint, validityCondition.condition)
+  );
+  if (failedConditions.length) {
+    const message = failedConditions[0]
+      ? failedConditions[0].invalidMessage
+      : "Invalid position.";
+    state.act.logMessage({ message });
+    return;
+  }
+
   const { cost } = blueprint.blueprint;
   if (state.select.canAffordToPay(cost.resource, cost.amount)) {
     state.act.removeEntity(entity.id);
