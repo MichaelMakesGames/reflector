8ce07da9c407e2f7975e0de0ec6f3b49b4ccb1dc
Michael Moore <mscottmoore@protonmail.com>
2022-01-12 19:16:23 -0600

    Revised building categories

--- src/data/buildingCategories.ts
+++ src/data/buildingCategories.ts
@@ -1,8 +1,9 @@
 import { TemplateName } from "../types/TemplateName";
 
 export enum BuildingCategoryCode {
-  Production = "PRODUCTION",
-  Laser = "LASER",
+  Work = "WORK",
+  Power = "POWER",
+  Defense = "DEFENSE",
   Misc = "MISC",
 }
 
@@ -15,9 +16,9 @@ export interface BuildingCategory {
 
 const buildingCategories: BuildingCategory[] = [
   {
-    code: BuildingCategoryCode.Production,
-    label: "Production",
-    description: "Buildings that produce resources.",
+    code: BuildingCategoryCode.Work,
+    label: "Work",
+    description: "Buildings that provide jobs for your colonists.",
     blueprints: [
       "BLUEPRINT_FARM",
       "BLUEPRINT_MINING_SPOT",
@@ -29,29 +30,39 @@ const buildingCategories: BuildingCategory[] = [
     ],
   },
   {
-    code: BuildingCategoryCode.Laser,
-    label: "Laser",
-    description: "Buildings that manipulate lasers.",
+    code: BuildingCategoryCode.Power,
+    label: "Power",
+    description: "Buildings that produce power.",
     blueprints: [
+      "BLUEPRINT_WINDMILL",
+      "BLUEPRINT_SOLAR_PANEL",
+      "BLUEPRINT_REACTOR",
+    ],
+  },
+  {
+    code: BuildingCategoryCode.Defense,
+    label: "Defense",
+    description: "Defense and laser-manipulating buildings.",
+    blueprints: [
+      "BLUEPRINT_WALL",
       "BLUEPRINT_PROJECTOR_BASIC",
       "BLUEPRINT_PROJECTOR_ADVANCED",
       "BLUEPRINT_SPLITTER_HORIZONTAL",
       "BLUEPRINT_SPLITTER_ADVANCED",
       "BLUEPRINT_ABSORBER",
+      "BLUEPRINT_SHIELD_GENERATOR",
     ],
   },
   {
     code: BuildingCategoryCode.Misc,
     label: "Misc",
-    description: "Miscellaneous buildings.",
+    description: "Housing, storage, and infrastructure.",
     blueprints: [
       "BLUEPRINT_TENT",
       "BLUEPRINT_RESIDENCE",
       "BLUEPRINT_ROAD",
       "BLUEPRINT_BATTERY",
       "BLUEPRINT_WAREHOUSE",
-      "BLUEPRINT_WALL",
-      "BLUEPRINT_SHIELD_GENERATOR",
     ],
   },
 ];
