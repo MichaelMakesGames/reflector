62095df76f449b33874213dc9409fcd1d549afd5
Michael Moore <mscottmoore@protonmail.com>
2022-01-19 20:14:03 -0600

    Enable Ctrl+Z in game over window

--- src/ui/GameOver.tsx
+++ src/ui/GameOver.tsx
@@ -5,8 +5,10 @@ import selectors from "../state/selectors";
 import Modal from "./Modal";
 import HotkeyButton from "./HotkeyButton";
 import { ControlCode } from "../types/ControlCode";
-import { HotkeyGroup } from "./HotkeysProvider";
+import { HotkeyGroup, useControl } from "./HotkeysProvider";
 import { load } from "../lib/gameSave";
+import { MAX_TURNS_TO_SAVE } from "../constants";
+import Kbd from "./Kbd";
 
 export default function GameOver({
   navigateTo,
@@ -20,6 +22,16 @@ export default function GameOver({
   const morale = useSelector(selectors.morale);
   const population = useSelector(selectors.population);
   const turn = useSelector(selectors.turn);
+
+  useControl({
+    code: ControlCode.Undo,
+    group: HotkeyGroup.GameOver,
+    callback: () =>
+      load(`save-${turn - 1}`)
+        .catch(() => null)
+        .then((state) => dispatch(actions.undoTurn(state ?? null))),
+    disabled: !gameOver,
+  });
 
   if (!gameOver) return null;
 
@@ -42,6 +54,12 @@ export default function GameOver({
       )}
       {population === 0 && (
         <p>All of your colonists died. Keep them defended!</p>
+      )}
+      {!victory && (
+        <p>
+          Hint: you can press <Kbd>Ctrl+Z</Kbd> to undo your turn, up to{" "}
+          {MAX_TURNS_TO_SAVE} times
+        </p>
       )}
       <div className="mt-1">
         <HotkeyButton
